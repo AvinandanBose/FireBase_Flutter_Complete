e11dee2da9625485437a7a1878756edb5ce89254
AvinandanBose <boseavinandan@gmail.com>
2022-08-06 02:53:47 +0530

    FirstEntry

---
+++ lib/Next.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class NextPage extends StatefulWidget {
+  const NextPage({Key? key}) : super(key: key);
+
+  @override
+  State<NextPage> createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+    );
+  }
+}
